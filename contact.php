479170a0bf22612834fa4a23610d0a72ad2d8803
Aashish Negi <111633419+NegiBeast@users.noreply.github.com>
2023-07-22 20:03:48 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,20 +1,19 @@
 <?php
+$con = mysqli_connect('localhost', 'id21058491_contact_info', 'Aashishnegi@24', 'id21058491_contactinginfo');
 
-    $con= mysqli_connect('localhost','id21058491_contact_info','Aashishnegi@24','id21058491_contactinginfo');
+$txtName = $_POST['txtName'];
+$txtEmail = $_POST['txtEmail'];
+$txtPhone = $_POST['txtPhone'];
+$txtMessage = $_POST['txtMessage'];
+$startVal=0;
 
-    $txtName=$_POST['txtName'];
-    $txtEmail=$_POST['txtEmail'];
-    $txtPhone=$_POST['txtPhone'];
-    $txtMessage=$_POST['txtMessage'];
+$sql = "INSERT INTO `ContactCredentials` (`fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
 
-    $sql="INSERT INTO 'ContactCredentials' ('fldName', 'fldEmail', 'fldPhone', 'fldMessage') VALUES ('$txtName','$txtEmail','$txtPhone','$txtMessage')";
+$rs = mysqli_query($con, $sql);
 
-    //rs = Result Set
-    $rs=mysqli_query($con,$sql);
+if ($rs) {
+    echo "Contact Records Inserted";
+}
 
-    if($rs){
-        echo "Contacts Records Inserted";
-    }
-
-
+mysqli_close($con);
 ?>
